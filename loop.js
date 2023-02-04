b9f3573012437d3776f4678712569a757092549f
Enamul <muhammad.enamul.haque79@gmail.com>
2023-02-04 09:59:21 +0600

    second commit

--- loop.js
+++ loop.js
@@ -1,9 +1,9 @@
 //for loop;
 
-// for(let i = 1; i <= 10; i++){
-//     console.log("Sorry!!");
+for(let i = 1; i <= 10; i++){
+    console.log("لاي الاها الله محمد رسول الله.");
     
-// }
+}
 
 //while loop;
 
@@ -13,12 +13,14 @@
 //     i++;
 // }
     
+//while loop;
 
-let n = 0;
-let x = 0;
+// let n = 0;
+// let x = 0;
 
-while (n < 3) {
-  n++;
-  x += n;
-  console.log(x);
-}
+// while (n < 3) {
+//   n++;
+//   x += n;
+//   console.log(x);
+// }
+
