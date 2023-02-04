d23b3b362cbb92a6e99ed8277b90e936cb554ae9
Enamul <muhammad.enamul.haque79@gmail.com>
2023-02-04 09:39:45 +0600

    first commit

---
+++ loop.js
@@ -0,0 +1,24 @@
+//for loop;
+
+// for(let i = 1; i <= 10; i++){
+//     console.log("Sorry!!");
+    
+// }
+
+//while loop;
+
+// let i = 1;
+// while(i <= 10){
+//     console.log("Sorry !!");
+//     i++;
+// }
+    
+
+let n = 0;
+let x = 0;
+
+while (n < 3) {
+  n++;
+  x += n;
+  console.log(x);
+}
