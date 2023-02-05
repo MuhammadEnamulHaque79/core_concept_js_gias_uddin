c0c83032c8c7935922845353b79789878dac8f58
Enamul <muhammad.enamul.haque79@gmail.com>
2023-02-05 23:40:32 +0600

    largestName function

--- largestName.js
+++ largestName.js
@@ -4,8 +4,8 @@ Alexis
 
 */
 
-let firstPersonName = "Muhammad Enamul Haque";
-let secondPersonName = "Salma Enam";
+// let firstPersonName = "Muhammad Enamul Haque";
+// let secondPersonName = "Salma Enam";
 
 // let myNameLength = firstPersonName.length;
 // let wifeNameLength = secondPersonName.length;
@@ -15,3 +15,44 @@ let secondPersonName = "Salma Enam";
 // }else{
 //     console.log(secondPersonName);
 // }
+
+// let firstPersonName = "Muhammad Enamul Haque";
+// let secondPersonName = "Salma Enam";
+
+// console.log(firstPersonName.length);
+// console.log(firstPersonName.length);
+
+// let myNameLength = firstPersonName.length;
+// let myWifeNameLength = secondPersonName.length;
+
+// if (myNameLength > myWifeNameLength) {
+//     console.log(firstPersonName.split("").reverse().join(""));
+// } else {
+//     console.log(secondPersonName.split("").reverse().join(""));
+// }
+
+function reverseName(firstPersonName, secondPersonName) {
+    
+    // let firstPersonName = "Muhammad Enamul Haque";
+    // let secondPersonName = "Salma Enam";
+
+    // console.log(firstPersonName.length);
+    // console.log(firstPersonName.length);
+
+    let myNameLength = firstPersonName.length;
+    let myWifeNameLength = secondPersonName.length;
+
+    if (myNameLength > myWifeNameLength) {
+        return firstPersonName.split("").reverse().join("");
+    } else {
+        return secondPersonName.split("").reverse().join("");
+    }
+     
+}
+
+const result = reverseName("Muhammad Enamul Haque","Salma Enam");
+console.log(result);
+
+
+
+
