ddd3a69bace3f5f3db7b56ca8fef2cf0a5090caf
Enamul <muhammad.enamul.haque79@gmail.com>
2023-02-06 00:38:41 +0600

    reverseName

--- largest_name.js
+++ largest_name.js
@@ -25,4 +25,25 @@ function reverseName(myName,myFriendName) {
 }
 
 let result = reverseName("Muhammad Enamul Haque", "Dr. Sakek Ahmed");
-console.log(result);
+// console.log(result);
+
+console.log(".....................");
+
+let myName ="Muhammad Enamul Haque";
+// console.log(myName);
+for(let i = 0; i < myName.length; i++){
+    // console.log(myName);
+    let element = myName[i];
+    console.log(element);
+}
+console.log("..................");
+//alternative way to finding reverse name/string;
+let myRealName ="Muhammad Enamul Haque";
+let reversedString = "";
+for(let i = myRealName.length - 1; i >= 0; i--){
+    let element = myRealName[i];
+    // console.log(element);
+    reversedString = reversedString + element;
+}
+
+console.log(reversedString);
