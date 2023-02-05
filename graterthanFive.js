9353b5e648309bc3762eaacde3e9c1bb0c94b2e3
Enamul <muhammad.enamul.haque79@gmail.com>
2023-02-05 22:02:55 +0600

    largestName finding

--- graterthanFive.js
+++ graterthanFive.js
@@ -29,24 +29,68 @@
 
 // console.log(count);
 
-let numbers = [-1, -2, -3, -4, -5, 6, 7, 8, -9, -10];
+// let numbers = [-1, -2, -3, -4, -5, 6, 7, 8, -9, -10];
 
-function greaterThanFive(){
-    let count = 0;
-   for(let i = 0; i <numbers.length; i++){
-        const element = numbers[i];
-        // console.log(element);
-        if(element > 5){
-            count++;
-            // count += 1;
-            // count = count + 1;
-            console.log("Those numbers are > 5 :", element);
-        }else{
-            continue;
-        }
-   }
-   return count;
-}
+// function greaterThanFive(){
+//     let count = 0;
+//    for(let i = 0; i <numbers.length; i++){
+//         const element = numbers[i];
+//         // console.log(element);
+//         if(element > 5){
+//             count++;
+//             // count += 1;
+//             // count = count + 1;
+//             console.log("Those numbers are > 5 :", element);
+//         }else{
+//             continue;
+//         }
+//    }
+//    return count;
+// }
 
-const output = greaterThanFive(numbers);
-console.log(output);
+// const output = greaterThanFive(numbers);
+// console.log("Total numbers > 5 are :", output);
+
+//odd numbers;
+
+// const array =[1,2,3,4,5,6,7,8,9,10];
+// let sum = 0;
+// let newArray = [];
+// for(let i = 1; i <= array.length; i++){
+    
+//     const element = array[i];
+//     // console.log(element);
+//     sum = sum + element;
+   
+//     if(element  % 2 == 1){
+//         console.log("odd numbers");
+//         array.push(element);
+//     }else{
+//         console.log("even numbers");
+//     }
+// }
+
+// function findOddNumbers(numbers){
+//     let sum = 0;
+//     let array = [];
+// for(let i = 1; i <= numbers.length; i++){
+    
+//     const element = array[i];
+//     // console.log(element);
+//     sum = sum + element;
+   
+//     if(element  % 2 == 1){
+//         console.log("element");
+//         array.push(element);
+//     }
+//     // else{
+//     //     // console.log("even numbers");
+//     //     continue;
+//     // }
+// }
+// return sum, array;
+// }
+
+// const array =[1,2,3,4,5,6,7,8,9,10];
+// const result = findOddNumbers(array);
+// console.log(result);
---
+++ largestName.js
@@ -0,0 +1,17 @@
+/*
+John
+Alexis
+
+*/
+
+let firstPersonName = "Muhammad Enamul Haque";
+let secondPersonName = "Salma Enam";
+
+// let myNameLength = firstPersonName.length;
+// let wifeNameLength = secondPersonName.length;
+
+// if(myNameLength > wifeNameLength){
+//     console.log(firstPersonName);
+// }else{
+//     console.log(secondPersonName);
+// }
